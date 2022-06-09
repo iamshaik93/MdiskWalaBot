475592e241366ccfb42d098a53dd55fcc4b26c0a
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-09 23:10:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ async def test(event):
         break
     if c <= 0:
         answer = f'''**No Results Found❗️
-Search Spelling On** **[Gₒₒgₗₑ](http://www.google.com/search?q={event.text.replace(' ', '%20')}%20Movie) 🔍**
+Search Spelling On** [Gₒₒgₗₑ](http://www.google.com/search?q={event.text.replace(' ', '%20')}%20Movie) 🔍
 '''
         buttons = None
         newbutton = [Button.url('Click To Check Spelling ✅', f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')], [Button.url('Click To Check Release Date 📅', f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')]
