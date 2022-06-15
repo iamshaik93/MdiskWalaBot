c9ba339acfa1114b2190975bf4d0e6a7e36d3f1a
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-15 12:50:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from telethon import Button
 from tmdbv3api import TMDb, Movie, TV
 from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-import mention
 
 # Bot Client for Inline Search
 Bot = Client(
