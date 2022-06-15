c96844ce8ccf2bb84befbce8b363229397323315
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-06-15 11:16:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -109,9 +109,9 @@ async def test(event):
     if await get_user_join(event.sender_id):
         pass
     else:
-        return await event.reply('''Hey! you need join My Updates Channel in order to use me 😍
-
-Press the Following Button to join Now 👇''', buttons=Button.url('🔉 Updates Channel', 'https://t.me/FYM_Update'))
+        return await event.reply('''**Hey! {} 😃**
+**Join Our Update Channel To Use Me**
+**Click Bellow Button To Join Now**''', buttons=Button.url('🔉 Updates Channel', 'https://t.me/FYM_Update'))
     search = client.iter_messages(Config.CHANNEL_ID, limit=10, search=args)
     answer = f'**📂 {event.text}**\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n'
     c = 0
