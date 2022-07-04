0fbffb32db15d99460b2c84c960d62d38847c171
kevinnadar <jesikamaraj@gmail.com>
2022-07-04 22:17:53 +0530

    master

--- configs.py
+++ configs.py
@@ -2,135 +2,71 @@
 
 import os
 
-if "DYNO" in os.environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
 
-if ON_HEROKU:
-    class Config(object):
-        API_ID = int(os.environ.get("API_ID", 12345))
-        API_HASH = os.environ.get("API_HASH", "")
-        BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-        BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchRobot")
-        USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-        CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-        BOT_USERNAME = os.environ.get("BOT_USERNAME")
-        BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-        UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-        DATABASE_URL = os.environ.get("DATABASE_URL", "")
-        LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-        RESULTS_COUNT = int(os.environ.get("RESULTS_COUNT", 5))
-        BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "True")
-        UPDATES_CHANNEL_USERNAME = os.environ.get("UPDATES_CHANNEL_USERNAME", "")
-        FORCE_SUB = os.environ.get("FORCE_SUB", "False")
-        AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", 300))
-        MDISK_API = os.environ.get("MDISK_API", "12334")
-        ABOUT_BOT_TEXT = """<b>This is Mdisk Search Robot.
-    🤖 My Name: <a href='https://t.me/MdiskSearchRobot'>Mdisk Search Robot</a>
+class Config(object):
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchRobot")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    RESULTS_COUNT = int(os.environ.get("RESULTS_COUNT", 5))
+    BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "True")
+    UPDATES_CHANNEL_USERNAME = os.environ.get("UPDATES_CHANNEL_USERNAME", "")
+    FORCE_SUB = os.environ.get("FORCE_SUB", "False")
+    AUTO_DELETE_TIME = int(os.environ.get("AUTO_DELETE_TIME", 300))
+    MDISK_API = os.environ.get("MDISK_API", "12334")
+    ABOUT_BOT_TEXT = """<b>This is Mdisk Search Robot.
+🤖 My Name: <a href='https://t.me/MdiskSearchRobot'>Mdisk Search Robot</a>
 
-    📝 Language : <a href='https://www.python.org'> Python V3</a>
+📝 Language : <a href='https://www.python.org'> Python V3</a>
 
-    📚 Library: <a href='https://docs.pyrogram.org'> Pyrogram</a>
+📚 Library: <a href='https://docs.pyrogram.org'> Pyrogram</a>
 
-    📡 Server: <a href='https://heroku.com'>Heroku</a>
+📡 Server: <a href='https://heroku.com'>Heroku</a>
 
-    👨‍💻 Created By: <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a></b>
-    """
+👨‍💻 Created By: <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a></b>
+"""
 
-        ABOUT_HELP_TEXT = """
-    <b>Send Your Group ID To Developer And Ask For Connect Your Group.
+    ABOUT_HELP_TEXT = """
+<b>Send Your Group ID To Developer And Ask For Connect Your Group.
 
-    Then Add This Bot To Your Group As Admin,
-    Then Send "/add_api Mdisk API" In Your Group.
+Then Add This Bot To Your Group As Admin,
+Then Send "/add_api Mdisk API" In Your Group.
 
-    Now All Links Are Converted.😍
-    When Users Ask For Any Movie In Your Group!
-    Bot Will Give Your Link To Them.🤑
+Now All Links Are Converted.😍
+When Users Ask For Any Movie In Your Group!
+Bot Will Give Your Link To Them.🤑
 
-    For More Information.👇
+For More Information.👇
 
-    👨‍💻 Developer : <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a></b>
-    """
+👨‍💻 Developer : <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a></b>
+"""
 
-        HOME_TEXT = """
-    Iꜰ Yᴏᴜ Lɪᴋᴇ Mᴇ!😘
+    HOME_TEXT = """
+Iꜰ Yᴏᴜ Lɪᴋᴇ Mᴇ!😘
 
-    Pʟᴇᴀꜱᴇ Sʜᴀʀᴇ Mᴇ Wɪᴛʜ Yᴏᴜʀ 
-    Fʀɪᴇɴᴅꜱ Aɴᴅ Fᴀᴍɪʟʏ.👨‍👨‍👧
+Pʟᴇᴀꜱᴇ Sʜᴀʀᴇ Mᴇ Wɪᴛʜ Yᴏᴜʀ 
+Fʀɪᴇɴᴅꜱ Aɴᴅ Fᴀᴍɪʟʏ.👨‍👨‍👧
 
-    Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna
-    """
+Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna
+"""
 
-        START_MSG = """
-    **Hᴇʏ! {}😅,
+    START_MSG = """
+**Hᴇʏ! {}😅,
 
-    I'ᴍ Mᴅɪꜱᴋ Sᴇᴀʀᴄʜ Rᴏʙᴏᴛ.🤖
+I'ᴍ Mᴅɪꜱᴋ Sᴇᴀʀᴄʜ Rᴏʙᴏᴛ.🤖
 
-    I Cᴀɴ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇꜱ Fᴏʀ Yᴏᴜ.🔍
+I Cᴀɴ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇꜱ Fᴏʀ Yᴏᴜ.🔍
 
-    Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna
+Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna
 
-    Want To Earn Money.🤑 /help 👈**
-    """
+Want To Earn Money.🤑 /help 👈**
+"""
 
 
-
-
-else:
-    class Config(object):
-        API_ID = 4106161
-        API_HASH = 'bf05f7a4f0a6ac3bc75afb4c89c44be6'
-        BOT_TOKEN ="5287186160:AAHbNHvvTQenrZW7-0oDO-5RfR8K60u3__A"
-        BOT_SESSION_NAME = "PyrogramBot"
-        USER_SESSION_STRING = '1AZWarzgBu3mS6kIUWvOrfrj79SU6qhx7Y1KuCs2Jrfxw2pX91KIyNo2YxOb7DZb8y9EVc7rwht8sRPGkW6gHxuDEeP8C5S4jjmZM0igB3gN-kY6devmvwemT0mqD27TuroCw2L6JSYws7DuI3TbAPSVLHrQjzoG6fuIDGtZ6Wp-K9P1tvhyn2Mm1dUWS0_i1FKjPS_vgEu2DqYTr_imlnVcUEkmqDwidDHT7SGi5oJTz3RJYsCXuYv3b4F_yCAjWoGKJnx7byJyArUQl27brHjpyj0QrjH6PAOCGvCXDEC2DcDLXDlOn4GH5kppOuaAGECDa8BZ4l8qQBgkHHKWFagP6COQi3Es='
-        CHANNEL_ID = -1001756552101
-        BOT_USERNAME = "Mdiskconvertorbot"
-        BOT_OWNER = 1861030649
-        LOG_CHANNEL = -1001194853986
-        # The URL of the MongoDB database.
-        DATABASE_URL = 'mongodb://localhost:27017'
-        BROADCAST_AS_COPY = "True"
-        UPDATES_CHANNEL = -1001635837706
-        UPDATES_CHANNEL_USERNAME = "T2linkss"
-        RESULTS_COUNT = 50
-        FORCE_SUB = "False"
-        AUTO_DELETE_TIME = 300
-        MDISK_API = '6LZq851sXoPHuwqgiKQq'
-        ABOUT_BOT_TEXT = """<b>This is Mdisk Search Robot.
-
-    🤖 My Name: <a href='https://t.me/MdiskSearchRobot'>Mdisk Search Robot</a>
-
-    📝 Language : <a href='https://www.python.org'> Python V3</a>
-
-    📚 Library: <a href='https://docs.pyrogram.org'> Pyrogram</a>
-
-    📡 Server: <a href='https://heroku.com'>Heroku</a>
-
-    👨‍💻 Created By: <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a></b>
-    """
-
-        ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a>
-
-    If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
-    """
-
-        HOME_TEXT = """
-    Iꜰ Yᴏᴜ Lɪᴋᴇ Mᴇ!😘
-
-    Pʟᴇᴀꜱᴇ Sʜᴀʀᴇ Mᴇ Wɪᴛʜ Yᴏᴜʀ 
-    Fʀɪᴇɴᴅꜱ Aɴᴅ Fᴀᴍɪʟʏ.👨‍👨‍👧
-
-    Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna
-    """
-
-
-        START_MSG = """
-    **Hᴇʏ! {}😅,
-
-    I'ᴍ Mᴅɪꜱᴋ Sᴇᴀʀᴄʜ Rᴏʙᴏᴛ.🤖
-
-    I Cᴀɴ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇꜱ Fᴏʀ Yᴏᴜ.🔍
-
-    Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ @RoyalKrrishna**
-    """
