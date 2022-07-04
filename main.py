0ca1ddd4d60906da03e473791d166c667e149fb4
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-05 01:17:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ async def message_handler(event):
                     f_text = await group_link_convertor(event.chat_id, f_text)
 
                 f_text = await link_to_hyperlink(f_text)
-                answer += f'\n\n**✅ PAGE {c}:**\n\n━━━━━━━━━\n\n\n' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
+                answer += f'\n\n\n\n**✅ PAGE {c}:**\n\n━━━━━━━━━\n\n\n\n' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
                     -1] 
                 
             # break
