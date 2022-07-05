6c97274c8f07880bf419fc4903f78f17da24bb30
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-07-05 18:25:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ async def message_handler(event):
                 c += 1
                 f_text = msg.text.replace("*", "")
 
-                if event.is_group or event.is_channel:
-                    f_text = await group_link_convertor(event.chat_id, f_text)
+#                 if event.is_group or event.is_channel:
+#                     f_text = await group_link_convertor(event.chat_id, f_text)
 
                 f_text = await link_to_hyperlink(f_text)
                 answer += f'\n\n**✅ PAGE {c}:**\n\n━━━━━━━━━\n\n' + '' + f_text.split("\n", 1)[0] + '' + '\n\n' + '' + f_text.split("\n", 2)[
