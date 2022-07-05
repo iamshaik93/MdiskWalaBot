6790006a1abe5712136cc1287e13bf8682d0377c
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-05 13:24:53 +0530

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -54,8 +54,7 @@ async def validate_q(q):
         return False
 
     # It removes the year from the search query.
-    query = re.sub(r"[1-2]\d{3}", "", query)
-    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|hd|movie|-|the|hindi|in|dubbed|\(|\)|dedo|print|full|bhai|latest|br((o|u)h?)*|hello|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", query.lower(), flags=re.IGNORECASE)
+    query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|new|hd|-|the|in|\(|\)|dedo|print|full|bhai|latest|br((o|u)h?)*|hello|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|kittum(o)*|aya(k)*(um(o)*)?|any(one)|with\ssubtitle(s)?)", "", query.lower(), flags=re.IGNORECASE)
     return query.strip()
 
 
