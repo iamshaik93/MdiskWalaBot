e5d0713fed8b3d40d6ae07e175ab1536a026946c
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-21 11:38:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,6 +12,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    BACKUP_CHANNEL = os.environ.get("BACKUP_CHANNEL")
     START_MSG = os.environ.get("START_MSG")
     HOME_TEXT = os.environ.get("HOME_TEXT")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
