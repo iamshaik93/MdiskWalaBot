b1624e07839178d09878c66c08eb541ebb3796b5
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-21 14:37:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -28,7 +28,7 @@ async def total_users(_, event: Message):
 @Client.on_message( filters.command("start") & filters.private)
 async def start_handler(_,event: Message):
     await event.reply_photo(
-        photo='https://telegra.ph/file/3dc2573762d700b63137f.jpg',
+        photo=Config.START_PHOTO,
         caption=Config.START_MSG.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton('Our Channel', url=f'https://t.me/{Config.UPDATES_CHANNEL_USERNAME}'),
