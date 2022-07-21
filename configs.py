952762e53edbe6eba9d06e2b1c82de919080c80f
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-07-21 10:54:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,6 +12,7 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")
+    START_MSG = os.environ.get("START_MSG", "")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
@@ -46,14 +47,4 @@ Pʟᴇᴀꜱᴇ Sʜᴀʀᴇ Mᴇ Wɪᴛʜ Yᴏᴜʀ
 Fʀɪᴇɴᴅꜱ Aɴᴅ Fᴀᴍɪʟʏ.👨‍👨‍👧
 
 Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ {OWNER_USERNAME}
-"""
-
-    START_MSG = """
-**Hᴇʏ! {}😅,
-
-I'ᴍ Mᴅɪꜱᴋ Sᴇᴀʀᴄʜ Rᴏʙᴏᴛ.🤖
-
-I Cᴀɴ Sᴇᴀʀᴄʜ Mᴏᴠɪᴇꜱ Fᴏʀ Yᴏᴜ.🔍
-
-Mᴀᴅᴇ Wɪᴛʜ ❤ Bʏ {OWNER_USERNAME}**
 """
