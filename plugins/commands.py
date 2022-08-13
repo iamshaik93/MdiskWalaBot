3945d84d0fba43e5f8d980aafb14097af8de3660
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-08-13 13:00:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ async def help_handler(_, event: Message):
              [
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ]
-             [InlineKeyboardButton("About", callback_data="About_msg")]
+             InlineKeyboardButton("About", url=f'https://t.me/{Config.ABOUT_TEXT}')]
         ])
     )
 
@@ -36,8 +36,8 @@ async def start_handler(_,event: Message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ],
 
-             [InlineKeyboardButton("About", callback_data="About_msg"),
-             InlineKeyboardButton("Help", callback_data="Help_msg")
+             [InlineKeyboardButton("About", url=f'https://t.me/{Config.ABOUT_TEXT}'),
+             InlineKeyboardButton("Help", url=f'https://t.me/{Config.HELP_TEXT}')
              ]
         ])
     )
