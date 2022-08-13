81a32250f5f9a573a86d6b71b8a8933963a38208
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-08-13 12:46:59 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -100,8 +100,8 @@ async def button(bot, cmd: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ],
                     [
-                        InlineKeyboardButton("About", callback_data="About_msg"),
-                        InlineKeyboardButton("Home", callback_data="gohome")
+                        InlineKeyboardButton("About", url=f'https://t.me/{Config.ABOUT_TEXT}'),
+                        InlineKeyboardButton("Help", url=f'https://t.me/{Config.HELP_TEXT}')
                     ]
                 ]
             ),
@@ -117,7 +117,7 @@ async def button(bot, cmd: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ], 
                                         [
-                        InlineKeyboardButton("About", callback_data="About_msg"),
+                        InlineKeyboardButton("About", url=f'https://t.me/{Config.ABOUT_TEXT}'),
                         InlineKeyboardButton("Home", callback_data="gohome")
                     ]
                 ]
@@ -134,8 +134,8 @@ async def button(bot, cmd: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
             ],
                     [
-                        InlineKeyboardButton("About", callback_data="About_msg"),
-                        InlineKeyboardButton("Help", callback_data="Help_msg")
+                        InlineKeyboardButton("About", url=f'https://t.me/{Config.ABOUT_TEXT}'),
+                        InlineKeyboardButton("Help", url=f'https://t.me/{Config.HELP_TEXT}')
                     ]
                 ]
             ),
