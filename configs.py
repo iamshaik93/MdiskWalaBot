85cf6af212a1a4dae962e0651bc0069cd9c69eb1
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-08-13 12:30:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -31,33 +31,5 @@ class Config(object):
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
     MDISK_API = os.getenv("MDISK_API", "12334")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "1"))
-    ABOUT_BOT_TEXT = """<b>This is Mdisk Search Robot.
-
-🤖 My Name: <a href='https://t.me/MdiskSearchRobot'>Mdisk Search Robot</a>
-
-📝 Language : <a href='https://www.python.org'> Python V3</a>
-
-📚 Library: <a href='https://docs.telethon.org'>Telethon</a>
-
-📡 Server: <a href='https://heroku.com'>Heroku</a></b>
-"""
-
-    ABOUT_HELP_TEXT = """
-<b>👨‍💻 Developer : <a href='https://t.me/RoyalKrrishna'>Royal Krrishna</a>
-
-Commands For Admin</b>
-
-/total_users
-/broadcast
-/connect
-/disconnect
-/add_api
-/remove_api
-/allow
-/disallow
-/channels
-/request
-/addb 
-/deny_access
-/give_access
-"""
+    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT")
+    ABOUT_HELP_TEXT = os.getenv("HELP_TEXT")
